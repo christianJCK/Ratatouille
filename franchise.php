ddbb97447472221d2dbc1246f0bdbb192dc386a4
Ducruet david <david.ducruet@orange.fr>
2016-09-13 18:11:04 +0200

    formulaire

--- franchise.php
+++ franchise.php
@@ -7,32 +7,144 @@
 	</head>
 	<body>
 		<?php include("header.php"); ?>
+		<div class="container-fluid">
 			<section>
 				<article>
-					<h1>Témoignages</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec sagittis massa. Nulla facilisi. Cras id arcu lorem, et semper purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel enim mi, in lobortis sem. Vestibulum luctus elit eu libero ultrices id fermentum sem sagittis. Nulla imperdiet mauris sed sapien dignissim id aliquam est aliquam. Maecenas non odio ipsum, a elementum nisi. Mauris non erat eu erat placerat convallis. Mauris in pretium urna. Cras laoreet molestie odio, consequat consequat velit commodo eu. Integer vitae lectus ac nunc posuere pellentesque non at eros. Suspendisse non lectus lorem.</p>
+					<div class="row">
+						<div class="col-xs-12 col-md-offset-1">
+							<h1>Avantages</h1>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12 col-md-offset-1 col-md-10 col-md-offset-1">
+							<p>
+								<ul>
+									<li>La notoriété du réseau : La franchise, c'est également une image dont vous bénéficierez au démarrage de votre activité. Les campagnes nationales du franchiseur ont un impact local sur les franchisés.</li>
+									<li>L’expertise et d’un savoir-faire expérimenté, Le franchiseur transmet son expérience et ce dans tous les domaines : expérience du terrain, création de dossier fiancier, gestion, management, marketing, etc.</li>
+									<li>L’expérience des autres franchisés : l’organisation en réseau permet à chaque franchisé de profiter et faire profiter les autres des expériences mutuelles. Cet échange d’expériences crée une véritable dynamique qui rayonne sur l’ensemble du réseau.</li>
+								</ul>
+							</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12 col-md-offset-1">
+							<h1>Témoignages</h1>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12 col-md-offset-1 col-md-10 col-md-offset-1">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec sagittis massa. Nulla facilisi. Cras id arcu lorem, et semper purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel enim mi, in lobortis sem. Vestibulum luctus elit eu libero ultrices id fermentum sem sagittis. Nulla imperdiet mauris sed sapien dignissim id aliquam est aliquam. Maecenas non odio ipsum, a elementum nisi. Mauris non erat eu erat placerat convallis. Mauris in pretium urna. Cras laoreet molestie odio, consequat consequat velit commodo eu. Integer vitae lectus ac nunc posuere pellentesque non at eros. Suspendisse non lectus lorem.</p>
+						</div>
+					</div>
 				</article>
 				<article>
-					<h1>Formulaire</h1>
+					<div class="row">
+						<div class="col-xs-12 col-md-offset-1">
+							<h1>Formulaire</h1>
+						</div>
+					</div>
 						<form>
 							<div class="row">
 								<div class="form-group">
-									<div class="col-xs-offset-0 col-xs-6 col-md-offset-1 col-md-3">
+									<div class="col-xs-12 col-md-offset-1 col-md-6">
 										<label for="Nom">Nom:</label>
 										<input type="Nom" class="form-control" id="Nom">
 									</div>
 								</div>
-									<div class="col-xs-6 col-md-8"></div>
+									<div class="col-md-5"></div>
 							</div>
 							<div class="row">
 								<div class="form-group">
-									<div class="col-xs-offset-0 col-xs-6 col-md-offset-1 col-md-3">
+									<div class="col-xs-12 col-md-offset-1 col-md-6">
 										<label for="Prénom">Prénom:</label>
 										<input type="Prénom" class="form-control" id="Prénom">
 									</div>
 								</div>
-									<div class="col-xs-6 col-md-8"></div>
+									<div class="col-md-5"></div>
 							</div>
+							<div class="row">
+								<div class="form-group">
+									<div class="col-xs-12 col-md-offset-1 col-md-6">
+										<label for="Adresse">Adresse:</label>
+										<input type="Adresse" class="form-control" id="Adresse">
+									</div>
+								</div>
+									<div class="col-md-5"></div>
+							</div>																	
+							<div class="row">
+								<div class="form-group">
+									<div class="col-xs-12 col-md-offset-1 col-md-6">					
+										<label for="Code_postal">Code postal:</label>
+										<input type="Code_postal" class="form-control" id="Code_postal">
+									</div>
+								</div>
+									<div class="col-md-5"></div>
+							</div>
+							<div class="row">
+								<div class="form-group">
+									<div class="col-xs-12 col-md-offset-1 col-md-6">											
+										<label for="Ville">Ville:</label>
+										<input type="Ville" class="form-control" id="Ville">
+									</div>
+								</div>
+									<div class="col-md-5"></div>
+							</div>
+							<div class="row">
+								<div class="form-group">
+									<div class="col-xs-12 col-md-offset-1 col-md-6">
+										<label for="Téléphone">Téléphone:</label>
+										<input type="Téléphone" class="form-control" id="Téléphone">
+									</div>
+								</div>
+									<div class="col-md-5"></div>
+							</div>
+							<div class="row">
+								<div class="form-group">
+									<div class="col-xs-12 col-md-offset-1 col-md-6">
+										<label for="E-mail">E-mail:</label>
+										<input type="E-mail" class="form-control" id="E-mail">
+									</div>
+								</div>
+									<div class="col-md-5"></div>
+							</div>
+							<div class="row">
+								<div class="form-group">
+									<div class="col-xs-12 col-md-offset-1 col-md-6">
+										<label for="Secteur">Secteur géographique souhaité:</label>
+										<input type="Secteur" class="form-control" id="Secteur">
+									</div>
+								</div>
+									<div class="col-md-5"></div>
+							</div>
+							<div class="row">
+								<div class="form-group">
+									<div class="col-xs-12 col-md-offset-1 col-md-6">
+										<label for="sel1">Apport personnel</label>
+										<select class="form-control" id="sel1">
+											<option>moins de 5000€</option>
+											<option>de 5000 à 10000€</option>
+											<option>de 10000 à 20000€</option>
+											<option>de 20000 à 30000€</option>
+											<option>plus de 30000€</option>
+										</select>
+									</div>
+								</div>
+									<div class="col-md-5"></div>
+							</div>
+							<div class="row">
+								<div class="form-group">
+									<div class="col-xs-12 col-md-offset-1 col-md-6">
+										<label for="comment">Votre projet:</label>
+										<textarea class="form-control" rows="5" id="comment"></textarea>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-offset-1 col-md-2">
+									<button type="submit" class="btn btn-default">ENVOYER</button>
+								</div>
+								<div class="col-md-9"></div>
+							</div>									
 						</form>
 				</article>
 			</section>
