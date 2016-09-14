2a6be0db32523b971cbf8d44a284b70927efa9ea
christianJCK <jecko.christian@gmail.com>
2016-09-14 11:06:10 +0200

    modif header

---
+++ footer.php
@@ -0,0 +1,27 @@
+		<footer>
+		<div class="row">
+			<div id="Contact" class="col-xs-offset-1 col-xs-10 col-md-offset-2 col-md-2">
+				<ul>
+					<li><h4>Contact<h4></li>
+					<li>Ratatouille restaurant</li>
+					<li>17 Rue Delandine</li>
+					<li>69003 LYON</li>
+					<li>E-mail: ratatouille.restaurant@wcs.com</li>
+					<li>La Loupe</li>
+				</ul>
+			</div>
+			<div id="Mentions_légales" class="col-xs-offset-1 col-xs-10 col-md-offset-1 col-md-2">
+				<h4>Mentions légales</h4>
+				<p>Tout droit réservés Ratatouille 2016</p>
+			</div>
+			<div id="nos_restaurants" class="col-xs-offset-1 col-xs-10 col-md-offset-1 col-md-2">
+				<ul>
+					<li>Lyon</li>
+					<li>Bordeaux</li>
+					<li>Fontainebleau</li>
+					<li>Orléans</li>
+					<li>Toulouse</li>
+					<li>La Loupe</li>
+				</ul>
+			</div>
+		</footer>
--- qui_sommes_nous.php
+++ qui_sommes_nous.php
@@ -15,14 +15,7 @@
 			</section>
 			</div>
 	</body>
-	<footer>
-		<div id="Contact">
-			<h4>Contact</h4>
-		</div>
-		<div id="Mentions_légales">
-			<h4>Mentions légales</h4>
-		</div>
-	</footer>
+	<?php include ("footer.php"); ?>
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
