18b25365bd20d4b738f9660765e6c2c6aef0f2d6
Ducruet david <david.ducruet@orange.fr>
2016-09-14 16:18:26 +0200

    rahhhhhhhhhhhhhhhhh2

--- footer.php
+++ footer.php
@@ -1,33 +1,33 @@
-		<footer>
-		<div class="row">
-			<div id="contact" class="col-xs-offset-1 col-xs-10 col-md-offset-2 col-md-2">
-				<ul>
-					<li><a class="contact" href="contact.php">Contact</a></li>
-					<li>17 Rue Delandine</li>
-					<li>69003 LYON</li>
-					<li>ratatouille.restaurant@wcs.com</li>
-				</ul>
-			</div>
-			<div id="Mentions_légales" class="col-xs-offset-1 col-xs-10 col-md-offset-1 col-md-2">
-				<a href="mentions.php" class="mentions">Mentions légales</a>
+<footer>
+	<div class="row">
+		<div id="contact" class="col-xs-offset-1 col-xs-10 col-md-offset-2 col-md-2">
+			<ul>
+				<li><a class="contact" href="contact.php">Contact</a></li>
+				<li>17 Rue Delandine</li>
+				<li>69003 LYON</li>
+				<li>ratatouille.restaurant@wcs.com</li>
+			</ul>
+		</div>
+
+		<div id="Mentions_légales" class="col-xs-offset-1 col-xs-10 col-md-offset-1 col-md-2">
+			<a href="mentions.php" class="mentions">Mentions légales</a>
 				<p>Tout droit réservés Ratatouille 2016</p>
-			</div>
-			<div id="nos_restaurants" class="col-xs-offset-1 col-xs-10 col-md-offset-1 col-md-2">
-				<!-- barre trad
-				<ul>
-					<li><a class="restaurants" href="restaurants.php">Nos restaurants</a></li>
-				</ul>
-				-->
-				<select name="Restaurants" class="bouton">
-  					<option><a href="http://www.lyon.fr/page/accueil.html">Lyon</a></option>
-  					<option>Orléans</option>
-  					<option>La loupe</option>
-  					<option>Toulouse</option>
-  					<option>Bordeaux</option>
-  					<option>Chartres</option>
-  					<option>Fontainebleau</option>
-				</select>
-
-
-			</div>
-		</footer>
+		</div>
+		<div id="nos_restaurants" class="col-xs-offset-1 col-xs-10 col-md-offset-1 col-md-2">
+		<!-- barre trad
+		<ul>
+			<li><a class="restaurants" href="restaurants.php">Nos restaurants</a></li>
+		</ul>
+		-->
+			<select name="Restaurants" class="bouton">
+				<option><a href="http://www.lyon.fr/page/accueil.html">Lyon</a></option>
+				<option>Orléans</option>
+				<option>La loupe</option>
+				<option>Toulouse</option>
+				<option>Bordeaux</option>
+				<option>Chartres</option>
+				<option>Fontainebleau</option>
+			</select>
+		</div>
+	</div>
+</footer>
--- index.php
+++ index.php
@@ -5,50 +5,41 @@
 		<link rel="stylesheet" href="ratatouille.css" />
 		<title>Ratatouille - Accueil</title>
 	</head>
-	<body>
-		<?php include("header.php"); ?>
-			<div class="container-fluid">
+	<?php include("header.php"); ?>
+	<body>		
+		<div class="container-fluid">
 			<section>
 				<article>
 					<div class="row">
 						<div class="col-xs-12 col-md-offset-1">
 							<h1>Cuisine traditionnelle</h1>
-
 						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12 col-md-offset-1 col-md-10 col-md-offset-1">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec sagittis massa. Nulla facilisi. Cras id arcu lorem, et semper purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel enim mi, in lobortis sem. Vestibulum luctus elit eu libero ultrices id fermentum sem sagittis. Nulla imperdiet mauris sed sapien dignissim id aliquam est aliquam. Maecenas non odio ipsum, a elementum nisi. Mauris non erat eu erat placerat convallis. Mauris in pretium urna. Cras laoreet molestie odio, consequat consequat velit commodo eu. Integer vitae lectus ac nunc posuere pellentesque non at eros. Suspendisse non lectus lorem.</p>
 
-						<div class="row">
-							<div class="col-xs-12 col-md-offset-1 col-md-10 col-md-offset-1">
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec sagittis massa. Nulla facilisi. Cras id arcu lorem, et semper purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel enim mi, in lobortis sem. Vestibulum luctus elit eu libero ultrices id fermentum sem sagittis. Nulla imperdiet mauris sed sapien dignissim id aliquam est aliquam. Maecenas non odio ipsum, a elementum nisi. Mauris non erat eu erat placerat convallis. Mauris in pretium urna. Cras laoreet molestie odio, consequat consequat velit commodo eu. Integer vitae lectus ac nunc posuere pellentesque non at eros. Suspendisse non lectus lorem.</p>
-
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec sagittis massa. Nulla facilisi. Cras id arcu lorem, et semper purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel enim mi, in lobortis sem. Vestibulum luctus elit eu libero ultrices id fermentum sem sagittis. Nulla imperdiet mauris sed sapien dignissim id aliquam est aliquam. Maecenas non odio ipsum, a elementum nisi. Mauris non erat eu erat placerat convallis. Mauris in pretium urna. Cras laoreet molestie odio, consequat consequat velit commodo eu. Integer vitae lectus ac nunc posuere pellentesque non at eros. Suspendisse non lectus lorem.</p>
-							</div>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec sagittis massa. Nulla facilisi. Cras id arcu lorem, et semper purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel enim mi, in lobortis sem. Vestibulum luctus elit eu libero ultrices id fermentum sem sagittis. Nulla imperdiet mauris sed sapien dignissim id aliquam est aliquam. Maecenas non odio ipsum, a elementum nisi. Mauris non erat eu erat placerat convallis. Mauris in pretium urna. Cras laoreet molestie odio, consequat consequat velit commodo eu. Integer vitae lectus ac nunc posuere pellentesque non at eros. Suspendisse non lectus lorem.</p>
 						</div>
-					</article>
-
-					<article>
-						<div class="row">
-							<div class="col-xs-12 col-md-offset-1">
-								<h1>Prochaines ouvertures</h1>
-							</div>
+					</div>
+				</article>
+				<article>
+					<div class="row">
+						<div class="col-xs-12 col-md-offset-1">
+							<h1>Prochaines ouvertures</h1>
 						</div>
+					</div>
 
-						<div class="row">
-							<div class="col-xs-12 col-md-offset-1 col-md-10 col-md-offset-1">
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec sagittis massa. Nulla facilisi. Cras id arcu lorem, et semper purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel enim mi, in lobortis sem. Vestibulum luctus elit eu libero ultrices id fermentum sem sagittis. Nulla imperdiet mauris sed sapien dignissim id aliquam est aliquam. Maecenas non odio ipsum, a elementum nisi. Mauris non erat eu erat placerat convallis. Mauris in pretium urna. Cras laoreet molestie odio, consequat consequat velit commodo eu. Integer vitae lectus ac nunc posuere pellentesque non at eros. Suspendisse non lectus lorem.</p>
+					<div class="row">
+						<div class="col-xs-12 col-md-offset-1 col-md-10 col-md-offset-1">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec sagittis massa. Nulla facilisi. Cras id arcu lorem, et semper purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel enim mi, in lobortis sem. Vestibulum luctus elit eu libero ultrices id fermentum sem sagittis. Nulla imperdiet mauris sed sapien dignissim id aliquam est aliquam. Maecenas non odio ipsum, a elementum nisi. Mauris non erat eu erat placerat convallis. Mauris in pretium urna. Cras laoreet molestie odio, consequat consequat velit commodo eu. Integer vitae lectus ac nunc posuere pellentesque non at eros. Suspendisse non lectus lorem.</p>
 
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec sagittis massa. Nulla facilisi. Cras id arcu lorem, et semper purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel enim mi, in lobortis sem. Vestibulum luctus elit eu libero ultrices id fermentum sem sagittis. Nulla imperdiet mauris sed sapien dignissim id aliquam est aliquam. Maecenas non odio ipsum, a elementum nisi. Mauris non erat eu erat placerat convallis. Mauris in pretium urna. Cras laoreet molestie odio, consequat consequat velit commodo eu. Integer vitae lectus ac nunc posuere pellentesque non at eros. Suspendisse non lectus lorem.</p>
-							</div>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec sagittis massa. Nulla facilisi. Cras id arcu lorem, et semper purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel enim mi, in lobortis sem. Vestibulum luctus elit eu libero ultrices id fermentum sem sagittis. Nulla imperdiet mauris sed sapien dignissim id aliquam est aliquam. Maecenas non odio ipsum, a elementum nisi. Mauris non erat eu erat placerat convallis. Mauris in pretium urna. Cras laoreet molestie odio, consequat consequat velit commodo eu. Integer vitae lectus ac nunc posuere pellentesque non at eros. Suspendisse non lectus lorem.</p>
 						</div>
-					</article>
-				</section>
-			</div>
-		
-
 					</div>
 				</article>
 			</section>
 		</div>
-
 	</body>
 	<?php include ("footer.php"); ?>
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
