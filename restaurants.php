bc001a5dc0f071ee53907968e746ba102414fa7d
Ducruet david <david.ducruet@orange.fr>
2016-09-16 11:46:52 +0200

    modif page restaurant

--- restaurants.php
+++ restaurants.php
@@ -9,14 +9,71 @@
 	</head>
 	<?php include("header.php"); ?>
 	<body>
-		<p>/!\ En cours de construction /!\</p>
-		<p><a name="C1"></a></p>
-		<p><a name="C2"></a></p>
-		<p><a name="C3"></a></p>
-		<p><a name="C4"></a></p>
-		<p><a name="C5"></a></p>
-		<p><a name="C6"></a></p>
-		<p><a name="C7"></a></p>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12 col-md-offset-1 col-md-2">
+					<a name="C1"><img src="http://i.imgur.com/uWQYewg.jpg"></a>
+					<p>Notre restaurant de lyon</p>
+				</div>
+				<div class="col-xs-12 col-md-8">		
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-md-offset-1 col-md-2">
+					<a name="C2"><img src="http://i.imgur.com/r6WrCwn.jpg"></a>
+					<p>Notre restaurant d'Orléans</p>
+				</div>
+				<div class="col-xs-12 col-md-8">		
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-md-offset-1 col-md-2">
+					<a name="C3"><img src="http://i.imgur.com/HHzqfPO.jpg"></a>
+					<p>Notre restaurant de La Loupe</p>
+				</div>
+				<div class="col-xs-12 col-md-8">		
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-md-offset-1 col-md-2">
+					<a name="C4"><img src="http://i.imgur.com/uOLyzNP.jpg"></a>
+					<p>Notre restaurant de toulouse</p>
+				</div>
+				<div class="col-xs-12 col-md-8">		
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-md-offset-1 col-md-2">
+					<a name="C5"><img src="http://i.imgur.com/JhdhfAe.jpg"></a>
+					<p>Notre restaurant de bordeaux</p>
+				</div>
+				<div class="col-xs-12 col-md-8">		
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-md-offset-1 col-md-2">
+					<a name="C6"><img src="http://i.imgur.com/1wNVHs4.jpg"></a>
+					<p>Notre restaurant de chartres</p>
+				</div>
+				<div class="col-xs-12 col-md-8">		
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-md-offset-1 col-md-2">
+					<a name="C7"><img src="http://i.imgur.com/bHDrJPF.jpg"></a>
+					<p>Notre restaurant de Fontainebleau</p>
+				</div>
+				<div class="col-xs-12 col-md-8">		
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+		</div>
 	</body>
 	<?php include ("footer.php"); ?>
 
